d532881f728d424b67b5ca3fc6da1caaa2596e57
ashraf1248 <aaldekaim@gmail.com>
2025-09-25 12:08:01 -0400

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,23 +1,38 @@
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function LandingPage() {
   const [films, setFilms] = useState([]);
+  const [actors, setActors] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/films/top")
       .then(res => res.json())
       .then(data => setFilms(data))
       .catch(err => console.error(err));
+
+    fetch("http://localhost:5000/api/actors/top")
+      .then(res => res.json())
+      .then(data => setActors(data))
+      .catch(err => console.error(err));
   }, []);
 
   return (
-    <div className="p-6">
-      <h2 className="text-2xl font-bold mb-4">Top 5 Rented Films</h2>
+    <div>
+      <h1>Top 5 Rented Films</h1>
       <ul>
         {films.map(film => (
           <li key={film.film_id}>
             <Link to={`/films/${film.film_id}`}>{film.title}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <h1>Top 5 Actors</h1>
+      <ul>
+        {actors.map(actor => (
+          <li key={actor.actor_id}>
+            <Link to={`/actors/${actor.actor_id}`}>{actor.name}</Link>
           </li>
         ))}
       </ul>
